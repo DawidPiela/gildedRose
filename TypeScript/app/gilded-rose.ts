d4ed6ec77250d3db6c60179fbafeb312ca56bb92
dawidbutter <dawid.piela@affirm.com>
2024-07-27 16:06:44 +0200

    Add conjured items

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -22,7 +22,12 @@ export class GildedRose {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
+            // Check if the item is Conjured
+            if (this.items[i].name.startsWith('Conjured')) {
+              this.items[i].quality = this.items[i].quality - 2
+            } else {
+              this.items[i].quality = this.items[i].quality - 1
+            }
           }
         }
       } else {
@@ -50,7 +55,12 @@ export class GildedRose {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
+                // Check if the item is Conjured
+                if (this.items[i].name.startsWith('Conjured')) {
+                  this.items[i].quality = this.items[i].quality - 2
+                } else {
+                  this.items[i].quality = this.items[i].quality - 1
+                }
               }
             }
           } else {
